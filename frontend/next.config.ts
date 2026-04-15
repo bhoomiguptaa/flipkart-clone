615e8abaa1d013129fe2bcfa86ee67c060efc30e
Bhoomi Gupta <bhoomi.gupta7708@gmail.com>
2026-04-16 04:33:01 +0530

    fix deployment config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: "standalone",
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "dummyimage.com",
       },
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.puma.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ssl-product-images.www8-hp.com",
+      }
     ],
   },
+
 };
 
 export default nextConfig;
